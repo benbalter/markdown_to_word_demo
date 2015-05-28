edbdecf05a09bd4a0eeb61432b4d9e00b37d3cd0
Ben Balter <ben.balter@github.com>
2015-05-28 10:12:08 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'markdown_to_word', :github => "benbalter/markdown_to_word"
+gem 'htmltoword', :github => "karnov/htmltoword"
+gem 'sinatra'
+gem 'rerun'
---
+++ server.rb
@@ -0,0 +1,25 @@
+require 'markdown_to_word'
+require 'sinatra'
+require 'digest'
+
+module MarkdownToWordServer
+  class App < Sinatra::Base
+
+    get "/" do
+      render_template :index, { :error => nil }
+    end
+
+    post "/" do
+      doc = MarkdownToWord.convert(params["markdown"])
+      content_type 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+      attachment "output.docx", :attachment
+      etag doc.hash
+      doc.contents
+    end
+
+    def render_template(template, locals={})
+      halt erb template, :layout => :layout, :locals => locals
+    end
+
+  end
+end
